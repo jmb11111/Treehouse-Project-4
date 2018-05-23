d69600dcca14e773bfe4e5e5e90ce66ac2f911f4
Josh <joshua.blasbalg@gmail.com>
2018-05-22 21:19:10 -0400

    Second attempt, eliminated Jquery plug in, made significant improvements to code organization, added name requirement

--- Project 4 Tic Tac Toe/tic-tac-toe-v3/js/script.js
+++ Project 4 Tic Tac Toe/tic-tac-toe-v3/js/script.js
@@ -1,28 +1,86 @@
 $('#player1').attr('class', 'players active');
-
-player1 = {
-    isTurn: true,
-    won: false
+const startGameButton = $("#startButton");
+class Player {
+    constructor(isTurn, won) {
+        this.isTurn = isTurn;
+        this.won = won;
+    }
 }
 
-player2 = {
-    isTurn: false,
-    won: false
+class Screen {
+    constructor(id, active) {
+        this.id = $("#"+id);
+        this.active = active;
+    }
+    showHide(){
+        if(this.active){
+            this.id.show();
+        }else if(this.active === false){
+            this.id.hide();
+        }
+    }
 }
 
-let tieGame = false;
+let gameStatus = {
+    player1wins : false,
+    player2Wins : false,
+    tieGame : false
+}
 
-let box0 = null;
-let box1 = null;
-let box2 = null;
-let box3 = null;
-let box4 = null;
-let box5 = null;
-let box6 = null;
-let box7 = null;
-let box8 = null;
-let box9 = null;
-let text = null;
+const player1 = new Player(true, false);
+const player2 = new Player(false, false);
+const tieGame = new Player(false, false);
+const screen1 = new Screen('screen1', true);
+const screen2 = new Screen("board", false);
+const screen3 = new Screen("finish", false);
+
+function showScreens(){
+screen1.showHide();
+screen2.showHide();
+screen3.showHide();
+}
+function screenWin(){
+    screen1.active = false;
+    screen2.active = false;
+    screen3.active = true;
+}
+
+function newGameScreen(){
+    screen1.active = false;
+    screen2.active = true;
+    screen3.active = false;
+}
+
+showScreens();
+
+
+startGameButton.click(function(){
+    if (document.getElementById("nameInput").value !== "") {
+    screen1.active = false;
+    screen2.active = true;
+    screen3.active= false;
+    showScreens();
+    }else {
+        alert("Please enter your name!")
+    }
+});
+
+
+let boxes = [ 
+ box0= null,
+ box1 = null,
+ box2 = null,
+ box3 = null,
+ box4 = null,
+ box5 = null,
+ box6 = null,
+ box7 = null,
+ box8 = null,
+
+ text = null,
+];
+
+
 $(".box").click(function (event) {
     if (player1.isTurn && !this.className.includes('box-filled')) {
         $(this).addClass('box-filled-1');
@@ -30,21 +88,18 @@ $(".box").click(function (event) {
         player2.isTurn = true;
         $('#player1').attr('class', 'players ');
         $('#player2').attr('class', 'players active ');
-        box0 = $("#box0").attr('class');
-        box1 = $("#box1").attr('class');
-        box2 = $("#box2").attr('class');
-        box3 = $("#box3").attr('class');
-        box4 = $("#box4").attr('class');
-        box5 = $("#box5").attr('class');
-        box6 = $("#box6").attr('class');
-        box7 = $("#box7").attr('class');
-        box8 = $("#box8").attr('class');
-        box9 = $("#box9").attr('class');
+
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i]=$("#box"+i.toString()).attr('class');
+        }
+    
         checkPlayer1Win();
         checkTie();
         if (player1.won) {
+            gameStatus.player1wins = true;
             gameOver();
-        }else if(tieGame === true){
+        } else if (tieGame.won) {
+            gameStatus.tieGame = true;
             gameOver();
         }
 
@@ -54,29 +109,24 @@ $(".box").click(function (event) {
         player2.isTurn = false;
         $('#player2').attr('class', 'players ');
         $('#player1').attr('class', 'players active');
-        box0 = $("#box0").attr('class');
-        box1 = $("#box1").attr('class');
-        box2 = $("#box2").attr('class');
-        box3 = $("#box3").attr('class');
-        box4 = $("#box4").attr('class');
-        box5 = $("#box5").attr('class');
-        box6 = $("#box6").attr('class');
-        box7 = $("#box7").attr('class');
-        box8 = $("#box8").attr('class');
-        box9 = $("#box9").attr('class');
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i]=$("#box"+i.toString()).attr('class');
+        }
         checkPlayer2Win();
         checkTie();
-            if (player2.won) {
-                gameOver();
-            }else if(tieGame === true){
-                gameOver();
-            }
+        if (player2.won) {
+            gameStatus.player2wins = true;
+            gameOver();
+        } else if (tieGame.won) {
+            gameStatus.tieGame = true;
+            gameOver();
+        }
     } else if (this.className.includes('box-filled')) {
         alert('That Spot Is Taken!!')
     }
-    
 
-    
+
+
 });
 
 
@@ -98,97 +148,91 @@ $(".box").hover(
 
 
 function checkPlayer1Win() {
-    if ((box0.includes("box-filled-1") && box1.includes("box-filled-1") && box2.includes("box-filled-1")) ||
-        (box0.includes("box-filled-1") && box3.includes("box-filled-1") && box6.includes("box-filled-1")) ||
-        (box0.includes("box-filled-1") && box4.includes("box-filled-1") && box8.includes("box-filled-1")) ||
-        (box1.includes("box-filled-1") && box4.includes("box-filled-1") && box7.includes("box-filled-1")) ||
-        (box2.includes("box-filled-1") && box5.includes("box-filled-1") && box8.includes("box-filled-1")) ||
-        (box2.includes("box-filled-1") && box4.includes("box-filled-1") && box6.includes("box-filled-1")) ||
-        (box3.includes("box-filled-1") && box4.includes("box-filled-1") && box5.includes("box-filled-1")) ||
-        (box6.includes("box-filled-1") && box7.includes("box-filled-1") && box8.includes("box-filled-1")))
+    if ((boxes[0].includes("box-filled-1") && boxes[1].includes("box-filled-1") && boxes[2].includes("box-filled-1")) ||
+        (boxes[0].includes("box-filled-1") && boxes[3].includes("box-filled-1") && boxes[6].includes("box-filled-1")) ||
+        (boxes[0].includes("box-filled-1") && boxes[4].includes("box-filled-1") && boxes[8].includes("box-filled-1")) ||
+        (boxes[1].includes("box-filled-1") && boxes[4].includes("box-filled-1") && boxes[7].includes("box-filled-1")) ||
+        (boxes[2].includes("box-filled-1") && boxes[5].includes("box-filled-1") && boxes[8].includes("box-filled-1")) ||
+        (boxes[2].includes("box-filled-1") && boxes[4].includes("box-filled-1") && boxes[6].includes("box-filled-1")) ||
+        (boxes[3].includes("box-filled-1") && boxes[4].includes("box-filled-1") && boxes[5].includes("box-filled-1")) ||
+        (boxes[6].includes("box-filled-1") && boxes[7].includes("box-filled-1") && boxes[8].includes("box-filled-1")))
         player1.won = true;
+        gameStatus.player1wins = true;
 }
 
 function checkPlayer2Win() {
     if (
-        (box0.includes("box-filled-2") && box1.includes("box-filled-2") && box2.includes("box-filled-2")) ||
-        (box0.includes("box-filled-2") && box3.includes("box-filled-2") && box6.includes("box-filled-2")) ||
-        (box0.includes("box-filled-2") && box4.includes("box-filled-2") && box8.includes("box-filled-2")) ||
-        (box1.includes("box-filled-2") && box4.includes("box-filled-2") && box7.includes("box-filled-2")) ||
-        (box2.includes("box-filled-2") && box5.includes("box-filled-2") && box8.includes("box-filled-2")) ||
-        (box2.includes("box-filled-2") && box4.includes("box-filled-2") && box6.includes("box-filled-2")) ||
-        (box3.includes("box-filled-2") && box4.includes("box-filled-2") && box5.includes("box-filled-2")) ||
-        (box6.includes("box-filled-2") && box7.includes("box-filled-2") && box8.includes("box-filled-2"))) {
+        (boxes[0].includes("box-filled-2") && boxes[1].includes("box-filled-2") && boxes[2].includes("box-filled-2")) ||
+        (boxes[0].includes("box-filled-2") && boxes[3].includes("box-filled-2") && boxes[6].includes("box-filled-2")) ||
+        (boxes[0].includes("box-filled-2") && boxes[4].includes("box-filled-2") && boxes[8].includes("box-filled-2")) ||
+        (boxes[1].includes("box-filled-2") && boxes[4].includes("box-filled-2") && boxes[7].includes("box-filled-2")) ||
+        (boxes[2].includes("box-filled-2") && boxes[5].includes("box-filled-2") && boxes[8].includes("box-filled-2")) ||
+        (boxes[2].includes("box-filled-2") && boxes[4].includes("box-filled-2") && boxes[6].includes("box-filled-2")) ||
+        (boxes[3].includes("box-filled-2") && boxes[4].includes("box-filled-2") && boxes[5].includes("box-filled-2")) ||
+        (boxes[6].includes("box-filled-2") && boxes[7].includes("box-filled-2") && boxes[8].includes("box-filled-2"))) {
         player2.won = true;
-    }}
-    function checkTie() {
-        if (
-            (box0.includes("box-filled") && box1.includes("box-filled") && box2.includes("box-filled")) &&
-            (box0.includes("box-filled") && box3.includes("box-filled") && box6.includes("box-filled")) &&
-            (box0.includes("box-filled") && box4.includes("box-filled") && box8.includes("box-filled")) &&
-            (box1.includes("box-filled") && box4.includes("box-filled") && box7.includes("box-filled")) &&
-            (box2.includes("box-filled") && box5.includes("box-filled") && box5.includes("box-filled")) &&
-            (box2.includes("box-filled") && box4.includes("box-filled") && box6.includes("box-filled")) &&
-            (box3.includes("box-filled") && box4.includes("box-filled") && box5.includes("box-filled")) &&
-            (box6.includes("box-filled") && box7.includes("box-filled") && box8.includes("box-filled"))) {
-            tieGame = true;
-        };
+        gameStatus.player2wins = true;
     }
-        
-
-    function gameOver() {
-            if
-            (player1.won) {
-                $("#finish").addClass('screen-win-one');
-                MultiScreen.switch_screens({ target_id: 'finish' });
-                document.getElementById("winner").innerHTML = text+"Wins!";
-            } else if (player2.won) {
-                $("#finish").addClass('screen-win-two');
-                MultiScreen.switch_screens({ target_id: 'finish' });
-                document.getElementById("winner").innerHTML = "X Wins!";
-            }else if (tieGame === true) {
-                $("#finish").addClass('screen-win-tie');
-                 MultiScreen.switch_screens({ target_id: 'finish' });
-                 document.getElementById("winner").innerHTML = "Tie Game!!";
-
-            }
-        }
+}
+function checkTie() {
+    if (
+        (boxes[0].includes("box-filled") && boxes[1].includes("box-filled") && boxes[2].includes("box-filled")) &&
+        (boxes[0].includes("box-filled") && boxes[3].includes("box-filled") && boxes[6].includes("box-filled")) &&
+        (boxes[0].includes("box-filled") && boxes[4].includes("box-filled") && boxes[8].includes("box-filled")) &&
+        (boxes[1].includes("box-filled") && boxes[4].includes("box-filled") && boxes[7].includes("box-filled")) &&
+        (boxes[2].includes("box-filled") && boxes[5].includes("box-filled") && boxes[5].includes("box-filled")) &&
+        (boxes[2].includes("box-filled") && boxes[4].includes("box-filled") && boxes[6].includes("box-filled")) &&
+        (boxes[3].includes("box-filled") && boxes[4].includes("box-filled") && boxes[5].includes("box-filled")) &&
+        (boxes[6].includes("box-filled") && boxes[7].includes("box-filled") && boxes[8].includes("box-filled"))) {
+        tieGame.won = true;
+        gameStatus.tieGame = true;
+    };
+}
 
 
-        function newGame() {
-            box0 = null;
-            box1 = null;
-            box2 = null;
-            box3 = null;
-            box4 = null;
-            box5 = null;
-            box6 = null;
-            box7 = null;
-            box8 = null;
-            box9 = null;
-            $("#box0").attr('class', 'box');
-            $("#box1").attr('class', 'box');
-            $("#box2").attr('class', 'box');
-            $("#box3").attr('class', 'box');
-            $("#box4").attr('class', 'box');
-            $("#box5").attr('class', 'box');
-            $("#box6").attr('class', 'box');
-            $("#box7").attr('class', 'box');
-            $("#box8").attr('class', 'box');
-            player1.won = false;
-            player2.won = false;
-            player1.isTurn = true;
-            $('#player1').attr('class', 'players active');
-            $('#player2').attr('class', 'players');
-            $("#finish").removeClass('screen-win-one');
-            $("#finish").removeClass('screen-win-two');
-            $("#finish").removeClass('screen-win-tie');
-            }
+function gameOver() {
+    if
+    (player1.won) {
+        $(".screen-win").attr('class' , 'screen screen-win screen-win-one');
+        screenWin();
+        showScreens();
+        document.getElementById("winner").innerHTML = text + "Wins!";
+    } else if (player2.won) {
+        $(".screen-win").attr('class' , 'screen screen-win screen-win-two');
+        screenWin();
+        showScreens();
+        document.getElementById("winner").innerHTML = "X Wins!";
+    } else if (tieGame.won) {
+        $(".screen-win").attr('class' , 'screen screen-win  screen-win-tie');
+      screenWin();
+        showScreens();
+        document.getElementById("winner").innerHTML = "Tie Game!!";
 
-            function getName() {
-                var x = document.getElementById("nameInput");
-                text = "";
-                text += x.value;
-                document.getElementById("player1Name").innerHTML = text;
-                
-            }
+    }
+}
+
+
+function newGame() {
+
+    for (let i = 0; i < boxes.length; i++) {
+        $("#box"+i.toString()).attr('class', 'box');
+        boxes[i]=null;
+    }
+    player1.won = false;
+    player2.won = false;
+    tieGame.won= false;
+    player1.isTurn = true;
+    newGameScreen();
+    showScreens();
+    $('#player1').attr('class', 'players active');
+    $('#player2').attr('class', 'players');
+
+}
+
+function getName() {
+    var x = document.getElementById("nameInput");
+    text = "";
+    text += x.value;
+    document.getElementById("player1Name").innerHTML = text;
+
+}
